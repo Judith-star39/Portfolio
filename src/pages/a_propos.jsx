ade3c571332d798149fc92b179fb17b14fd2b29d
judith <judithmawombo05@gmail.com>
2026-09-07 21:09:35 +0200

    modif cv

--- src/pages/a_propos.jsx
+++ src/pages/a_propos.jsx
@@ -5,7 +5,7 @@ export default function Apropos (){
         <section id="a-propos">
             <h2> À Propos</h2>
             <p>Développeuse front-end junior formée chez OpenClassrooms, je conçois des interfaces web modernes, accessibles et performantes avec HTML, CSS, JavaScript et React. Sensible à l'expérience utilisateur et à la qualité du code, je recherche aujourd'hui un CDI en Île-de-France afin de poursuivre ma progression et contribuer à des projets innovants au sein d'une équipe dynamique.</p>
-            <a href="/CV_judith_Mawombo.pdf" target="_blank" rel="noopener noreferrer" >Voir mon CV</a>
+            <a href="/CV_Judith_Mawombo.pdf" target="_blank" rel="noopener noreferrer" >Voir mon CV</a>
         </section>
     )
 }
